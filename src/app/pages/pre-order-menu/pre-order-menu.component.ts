a0d80a5bbefcaeb58d51d63243f59c9f58cfd367
Jacqueline Teixeira <teixeijf@gmail.com>
2021-09-18 18:19:24 -0300

    takeOutMenu and preOrder menu first changes

--- src/app/pages/pre-order-menu/pre-order-menu.component.ts
+++ src/app/pages/pre-order-menu/pre-order-menu.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from 'src/models/products.model';
+import { ProductsService } from 'src/services/products.service';
+
 @Component({
   selector: 'app-pre-order-menu',
   templateUrl: './pre-order-menu.component.html',
-  styleUrls: ['./pre-order-menu.component.scss']
+  styleUrls: ['./pre-order-menu.component.scss'],
 })
 export class PreOrderMenuComponent implements OnInit {
+  preOrderProducts: Product[] = [];
 
-  constructor() { }
+  constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
+    this.preOrderProducts = this.productsService.getPreOrderProducts();
   }
-
 }
--- src/app/pages/take-out-menu/take-out-menu.component.ts
+++ src/app/pages/take-out-menu/take-out-menu.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from 'src/models/products.model';
+import { ProductsService } from 'src/services/products.service';
 @Component({
   selector: 'app-take-out-menu',
   templateUrl: './take-out-menu.component.html',
-  styleUrls: ['./take-out-menu.component.scss']
+  styleUrls: ['./take-out-menu.component.scss'],
 })
 export class TakeOutMenuComponent implements OnInit {
+  takeOutProducts: Product[] = [];
 
-  constructor() { }
+  constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
+    this.takeOutProducts = this.productsService.getTakeOutProducts();
   }
-
 }
