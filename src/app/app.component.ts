1eede3cd30f8d5c60a3c7f3c511366a77f676578
Jacqueline Teixeira <teixeijf@gmail.com>
2021-09-18 18:44:50 -0300

    list menu change styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Product } from 'src/models/products.model';
-import { ProductsService } from 'src/services/products.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [ProductsService],
+  providers: [],
 })
 export class AppComponent implements OnInit {
   title = 'doces-di-fabi';
   loadedFeature = 'menu';
 
-  takeOutProducts: Product[] = [];
-  preOrderProducts: Product[] = [];
+  constructor() {}
 
-  constructor(private productsService: ProductsService) {}
-
-  ngOnInit(): void {
-    this.takeOutProducts = this.productsService.getTakeOutProducts();
-    this.preOrderProducts = this.productsService.getPreOrderProducts();
-  }
+  ngOnInit(): void {}
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
